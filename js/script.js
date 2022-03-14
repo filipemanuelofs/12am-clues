3e41c73e273beacef5dcc800cb0c1e741c58c30b
Filipe Manuel <filipemanuelofs@gmail.com>
2022-03-14 00:02:22 -0300

    Update index.html

--- js/script.js
+++ js/script.js
@@ -17,12 +17,3 @@ console.log(
 `,
   "font-family:monospace"
 );
-(function () {
-  var ua = window.navigator.userAgent;
-  var msie = ua.indexOf("MSIE ");
-  var trident = ua.indexOf("Trident/");
-  if (msie > 0 || trident > 0) {
-    console.log("IE detected.");
-    window.location.replace("unsupported.html");
-  }
-})();
